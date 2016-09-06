1f68014276866770947317c4b0ae94048cd532f0
rap2hpoutre <raphaelht@gmail.com>
2016-09-06 22:34:32 +0200

    Boom

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+cd /home/www/drum-beats
 git pull origin master
 cargo build --release
 
-npm run build
-
+(cd react-app && npm run build)
+cp -Rf react-app/build/static/* static/
+cat react-app/build/index.html > resources/index.html
 
 service supervisor restart

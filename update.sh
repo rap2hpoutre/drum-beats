13aa1e25595ab99b5964809c13d317d82cf17c64
rap2hpoutre <raphaelht@gmail.com>
2016-09-08 14:47:34 +0200

    Boom

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ cd /home/www/drum-beats
 git pull origin master
 cargo build --release
 
+(cd react-app && npm install)
 (cd react-app && npm run build)
 cp -Rf react-app/build/static/* static/
 cat react-app/build/index.html > resources/index.html
